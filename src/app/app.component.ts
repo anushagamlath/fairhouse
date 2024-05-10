a9c3cd22114a6c6bcf7090b8f35bda5aef837ddd
Anusha Gamlath <anugamlath@gmail.com>
2024-05-10 10:19:13 +0200

    ✨  search and view features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,50 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { HousingLocation } from './housing-location';
+import { HousingListComponent } from './housing-list/housing-list.component';
+import { CommonModule } from '@angular/common';
 @Component({
-  selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  selector: 'app-root',
+  imports:[HousingListComponent,CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'fairhouse';
+
+  housingLocationList: HousingLocation[] = [
+    {
+      name: "Acme Fresh Start Housing",
+      city: "Chicago",
+      state: "IL",
+      photo: "../assets/housing-1.jpg",
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      name: "A113 Transitional Housing",
+      city: "Santa Monica",
+      state: "CA",
+      photo: "../assets/housing-2.jpg",
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      name: "Warm Beds Housing Support",
+      city: "Juneau",
+      state: "AK",
+      photo: "../assets/housing-3.jpg",
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    }
+  ];
+
+  selectedLocation: HousingLocation | undefined;// = this.housingLocationList[0];
+
+  updateSelectedLocation(location: HousingLocation) {
+    this.selectedLocation = location;
+  }
 }
